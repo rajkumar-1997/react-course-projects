d63b6a8dc9a518786963bf48fa2406a65a8cd783
Rajkumar Jangam <rajkumarJangam66@gmail.com>
2024-05-31 20:10:44 +0530

    basic setup of react  understood

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Component from './Component';
+
+function App() {
+  return (
+    <>
+      <h1>React app</h1>
+      <Component />
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Component.jsx
@@ -0,0 +1,5 @@
+const Component = () => {
+  return <h1>This is react component</h1>;
+};
+
+export default Component;
